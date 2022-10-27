bb80c8f3aab616cf2250f64b45b2c34264a4007c
Raga <ragawilliam570@gmail.com>
2022-10-27 13:47:30 +0300

    Add Profile and LinkSection components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Profile from './components/Profile';
+import LinkSection from './components/LinkSection';
 
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <Profile />
-  </div>
+  <>
+    <div className="App">
+      <Profile />
+    </div>
+    <LinkSection />
+  </>
 );
 
 export default App;
