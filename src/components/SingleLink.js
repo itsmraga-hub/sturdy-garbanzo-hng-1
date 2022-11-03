1f53a929b4383f259a16b0483b56a31b01459816
Raga <ragawilliam570@gmail.com>
2022-11-03 13:00:47 +0300

    configure route with Link

--- src/components/SingleLink.js
+++ src/components/SingleLink.js
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const SingleLink = (props) => {
   const p = props;
   const { name, id, link } = p;
   return (
     <li>
       {
-        id !== 'contact' ? <a href={link} id={id} target="_blank" rel="noreferrer">{name}</a> : <a href={link} id={id} rel="noreferrer">{name}</a>
+        id !== 'contact' ? <a href={link} id={id} target="_blank" rel="noreferrer">{name}</a> : (
+          <Link to="/contact">
+            <a href={link} id={id} rel="noreferrer">{name}</a>
+          </Link>
+        )
       }
     </li>
   );
